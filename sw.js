67d4c1ef3b0409918f48eee1a73eca252fe1d7b4
Melvinhooo <msejdiu@b-dhilden.de>
2026-04-27 23:30:42 +0200

    Reine NBA-Nacht 27.04. (3 Spiele, 6 Einzeltipps, 4 Kombis) + SW v10

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * So hast du offline die letzte App-Version + die letzten Daten.
  */
 
-const CACHE = 'miesmuschel-v9';
+const CACHE = 'miesmuschel-v10';
 const SHELL = [
   './',
   './index.html',
